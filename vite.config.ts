98442ae22e1de8b4681c69cb76dc5af3e977e846
Sorakield <sorakield@gmail.com>
2025-05-10 16:08:36 +0300

    Исправление ошибки загрузки модулей

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,13 @@ import { defineConfig } from 'vite'
 export default defineConfig({
 	plugins: [react()],
 	base: '/',
+	build: {
+		outDir: 'dist',
+		assetsDir: 'assets',
+		rollupOptions: {
+			output: {
+				manualChunks: undefined,
+			},
+		},
+	},
 })
